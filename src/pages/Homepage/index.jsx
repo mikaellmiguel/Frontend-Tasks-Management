050a3cb30a3bacccf1cacc3afffb5458cda80ceb
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-02-23 14:04:44 -0300

    chore: instalação do react router dom

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -3,34 +3,9 @@ import { Container, BoardContainer, Header, Options} from "./styles";
 import { Input } from "../../components/Input";
 import { Board } from "../../components/Board";
 import {api} from "../../services/api";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Homepage() {
-
-  const tasksPendente = [
-   {id: 1, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Alta", status: "pendente"},
-   {id: 2, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "pendente"},
-   {id: 3, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "pendente"},
-   {id: 4, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "pendente"},
-   {id: 5, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "pendente"},
-  ];
-
-  const tasksRealizando = [
-    {id: 1, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Alta", status: "realizando"},
-    {id: 2, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "realizando"},
-    {id: 3, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "realizando"},
-    {id: 4, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "realizando"},
-    {id: 5, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "realizando"},
-  ];
-
-  const tasksConcluido = [
-    {id: 1, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Alta", status: "concluída"},
-    {id: 2, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "concluída"},
-    {id: 3, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "concluída"},
-    {id: 4, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "concluída"},
-    {id: 5, title: "Estudar React", description: "Estudar React para entender melhor o funcionamento", priority: "Média", status: "concluída"},
-  ];
-
 
   const [tasks, setTasks] = useState([]);
 
